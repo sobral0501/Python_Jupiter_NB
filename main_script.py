39ed9de023de951152641315b9e5cecccc65e592
Djair Sobral <69758846+sobral0501@users.noreply.github.com>
2021-09-19 19:45:05 -0500

    Add files via upload

---
+++ main_script.py
@@ -0,0 +1,26 @@
+
+# [ ] The following program asks the user for a circle radius then display the area and circumference
+# Modify the program so it only displays the information when executed directly
+# The program should not display anything if it is imported as a module 
+
+def main():
+    import sys
+    from math import pi
+
+    def circle_area(r):
+        return pi * (r ** 2)
+
+    def circle_circumference(r):
+        return  2 * pi * r
+
+    radius = float(input("Enter radius: "))
+    print("Area =", circle_area(radius))
+    print("Circumference =", circle_circumference(radius))
+
+if __name__ == "__main__":
+    main()
+
+print('Djair Attacking')
+
+print('Djair Attacking')
+Print('One More line of program')
